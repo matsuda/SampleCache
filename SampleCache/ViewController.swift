ed312ea831264215d0378e3e4007ef59468eb4bb
matsuda <kosukematsuda@gmail.com>
2020-03-15 17:56:42 +0900

    Add sample code to get web image

--- SampleCache/ViewController.swift
+++ SampleCache/ViewController.swift
@@ -8,13 +8,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
+
+    @IBOutlet private weak var imageView: UIImageView!
+    private let imageURLs: [String] = [
+        "https://folio-sec.com/images/theme/themeBoard/vr.jpg",
+        "https://folio-sec.com/images/theme/themeBoard/sushi.jpg",
+        "https://folio-sec.com/images/theme/themeBoard/pet.jpg",
+        "https://folio-sec.com/images/theme/themeBoard/kyoto.jpg",
+    ]
+    private var number = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func tapButton(_ sender: Any) {
+        let url = imageURLs[number]
+        fetchImage(with: url)
+        number += 1
+        if number >= imageURLs.count {
+            number = 0
+        }
+    }
 
+    @IBAction func tapButton2(_ sender: Any) {
+        UIApplication.shared.perform(Selector(("_performMemoryWarning")))
+    }
 }
 
+extension ViewController {
+    func fetchImage(with urlString: String) {
+        let url = URL(string: urlString)!
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard error == nil else { return }
+            guard let data = data else { return }
+            guard let image = UIImage(data: data) else { return }
+            DispatchQueue.main.async { [weak self] in
+                self?.imageView.image = image
+            }
+        }.resume()
+    }
+}
